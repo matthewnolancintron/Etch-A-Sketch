1a3e3ee0935165495c500cfd2e86372f54894364
Matthew Shane Nolan-Cintron <matthewnolancintron@gmai.com>
2021-07-06 21:47:01 -0400

    added a button that clears the current grid and prompts the user for the number of squares per side of the new grid to be created

--- sketch.js
+++ sketch.js
@@ -1,29 +1,62 @@
-//listner for when the dom is ready and the call etch
-document.addEventListener('DOMContentLoaded', etch);
-
 /*
-    0:
-    split code into functions
-
-    1:
+    todo:
     remove inline styles to a separate style sheets
     and add class/ids with js instead
  */
 
-//main function of the program
-function etch(){
-    // create 16x16 grid of square divs
+// 
+let grid;
 
+//listen for when the dom is ready and then call etch
+document.addEventListener('DOMContentLoaded', etch);
+
+//main function of the program
+function etch() {
+    // create 16x16 grid of square divs
+    grid = makeGrid(16);
+
+    //create button for creating a new grid
+    const createNewGridButton = document.createElement('button');
+
+    //add content to button and style it
+    createNewGridButton.textContent = 'create new grid';
+    createNewGridButton.style.marginBottom = '2%';
+    
+    //functionality
+    createNewGridButton.addEventListener('click', () => {
+        clearGrid(grid);
+    });
+
+    createNewGridButton.addEventListener('click', createNewGrid);
+
+
+    //add button to the page
+    document.body.insertBefore(createNewGridButton, grid);
+    // extra
+    /*
+       each pass with mouse over the square changes it to
+       completely random rgb value so each square is a random
+       color
+
+       try having each pass just add another 10% black
+       so that after 10 passes the square is completely black
+     */
+}
+
+function makeGrid(numSquaresPerSide) {
     // contains all the gridSquares
-    const grid = document.createElement('div');
+    let grid = document.createElement('div');
 
     // creates all the grid squares and add them to the grid
-    for(let i = 0; i < (16*16); i++){
+    for (let i = 0; i < (numSquaresPerSide * numSquaresPerSide); i++) {
         //create a square
         let gridSquare = document.createElement('div');
 
         //style the square
-        gridSquare.style.border = 'solid black 1px';
+        gridSquare.style.border = 'solid black 0.1em';
+
+        //    gridSquare.style.width = '1%';
+        //    gridSquare.style.height = '1%';
 
         gridSquare.style.backgroundColor = 'white';
 
@@ -34,6 +67,11 @@ function etch(){
         // as the background
         gridSquare.style.color = 'white';
 
+        //
+        gridSquare.style.width = `${1 / grid.style.width}%`;
+        gridSquare.style.height = `${1 / grid.style.height}%`;;
+        gridSquare.style.overflow = 'hidden';
+
         //grid squares changes color when mouse passes over
         gridSquare.addEventListener("mouseenter", () => {
             gridSquare.style.backgroundColor = 'grey';
@@ -43,18 +81,61 @@ function etch(){
         // add sqaure to the grid
         grid.appendChild(gridSquare);
     }
-
-    // add grid to the body
-    document.body.appendChild(grid);
-
-    //use css to shape divs in div container into grid
-    //flex or css grid
-
+    //style the grid
+    grid.style.border = 'solid green 2px';
     //make grid into a grid
     grid.style.display = 'grid';
-    grid.style.gridTemplateColumns = 'repeat(16,1fr)';
-    grid.style.gridTemplateRows = 'repeat(16,1fr)';
+    grid.style.gridTemplateColumns = `repeat(${numSquaresPerSide},1fr)`;
+    grid.style.gridTemplateRows = `repeat(${numSquaresPerSide},1fr)`;
 
-    
+    // set size of the grid
+    grid.style.width = '700px';
+    grid.style.height = '300px';
+
+    // grid to the page
+    //if there is a grid already present replace it
+    if (document.body.querySelector('div') != null) {
+        document.body.replaceChild(grid, document.body.querySelector('div'));
+    } else {
+        // if not add a new grid
+        // add grid to the body 
+        document.body.appendChild(grid);
+    }
+
+    return grid;
 }
+
+function clearGrid(grid) {
+grid.childNodes.forEach(element => {
+    element.style.backgroundColor = 'white';
+    element.style.color = 'white';
+    });
+}
+
+
+//create a new grid based on user input
+function createNewGrid() {
+    //prompt users for the number of sqaures per side
+    let ChosenNumberOfSquaresPerSide = prompt('how many squares per side?', 'input one number, max is 100');
+    console.log(ChosenNumberOfSquaresPerSide);
+
+    //make sure input is valid
+    //only if input if valid
+    if (ChosenNumberOfSquaresPerSide != null) {
+        //convert user input from a string into a number
+        let numberOfSquaresPerSide = Number(ChosenNumberOfSquaresPerSide);
+        // if it can not be converted then number returns NaN
+        if (numberOfSquaresPerSide != NaN) {
+            //check if the number is equal to or less than 100
+            // need to check for negative numbers
+            if (numberOfSquaresPerSide <= 100 && numberOfSquaresPerSide > 0) {
+                //call function that creates grid
+                grid = makeGrid(numberOfSquaresPerSide);
+
+            }
+        }
+
+    }
+}
+
 
